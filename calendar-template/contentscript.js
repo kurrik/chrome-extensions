0f791e79033ce235c24284c8b67f11fca18e6a28
Arne Roomann-Kurrik <kurrik@gmail.com>
2010-10-18 17:40:29 -0700

    Update sample to use the DOMNodeInserted mutation event.  Add an options page for customizing the template.

--- calendar-template/contentscript.js
+++ calendar-template/contentscript.js
@@ -3,5 +3,43 @@
  * source code is governed by a BSD-style license that can be found in the
  * LICENSE file.
  */
-chrome.extension.sendRequest({action: "display"}, function(response) {
+
+function getTextInput(evt) {
+  if (evt.target.querySelector) {
+    if (evt.target.nodeName == 'TEXTAREA' &&
+        evt.target.classList.contains('textinput')) {
+      return evt.target;
+    } else {
+      return evt.target.querySelector('textarea.textinput');
+    }
+  }
+};
+
+var template = false;
+
+function onDomInserted(evt) {
+  if (!template || template == "") {
+    return;
+  }
+  var input = getTextInput(evt);
+  if (input) {
+    var parent = input.parentNode && input.parentNode.parentNode;
+    if (parent && parent.classList.contains('ep-dp-descript')) {
+      if (input.value == "") {
+        input.value = template;
+      } else if (input.value == "Click to add a description") {
+        input.value = template;
+        input.classList.remove('ui-placeholder');
+        input.addEventListener('focus', function(evt) {
+          evt.stopImmediatePropagation();
+        }, true);
+      }
+    }
+  }
+};
+
+chrome.extension.sendRequest({get: "template"}, function(response) {
+  template = response.template;
+  document.body.addEventListener('DOMNodeInserted', onDomInserted, false);
+  console.log('Added listener');
 });
--- calendar-template/inserttemplate.js
+++
@@ -1,5 +0,0 @@
-var input = document.querySelector('textarea.textinput');
-console.log(input);
-if (input) {
-  input.innerText = "Foo bar";
-}
